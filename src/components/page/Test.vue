eeaa0622eb314fe4c260a0fc420972e02aff453d
JerryYZR <837468381@qq.com>
2019-08-12 20:59:01 +0800

    first commit

---
+++ src/components/page/Test.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="table">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-tickets"></i> 基础表格</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="handle-box">
+                <el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">
+                    <el-option key="1" label="广东省" value="广东省"></el-option>
+                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                </el-select>
+                <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+                <el-button type="primary" icon="search" @click="search">搜索</el-button>
+            </div>
+            <el-table :data="tableData.slice((cur_page-1)*pagesize,cur_page*pagesize)" border style="width: 100%" ref="multipleTable">
+                <el-table-column prop="date" label="日期" sortable width="150">
+                </el-table-column>
+                <el-table-column prop="name" label="姓名" width="120">
+                </el-table-column>
+                <el-table-column prop="address" label="地址" :formatter="formatter">
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination 
+                @current-change="handleCurrentChange" 
+                layout="total, sizes, prev, pager, next, jumper" 
+                :page-size="pagesize" 
+                :total="100">
+                </el-pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'basetable',
+        data() {
+            return {
+                url: './static/vuetable.json',
+                tableData: [],
+                pagesize: 5,
+                cur_page: 1,
+                select_cate: '',
+                select_word: '',
+                is_search: false,
+                idx: -1
+            }
+        },
+        created() {
+            this.getData();
+        },
+        computed: {
+            data() {
+                return this.tableData.filter((d) => {
+                    if (d.address.indexOf(this.select_cate) > -1 &&
+                            (d.name.indexOf(this.select_word) > -1 ||
+                                d.address.indexOf(this.select_word) > -1)
+                        ) {
+                            return d;
+                        }
+                })
+            }
+        },
+        methods: {
+            // 分页导航
+            handleCurrentChange(val) {
+                this.cur_page = val;
+                this.getData();
+            },
+            // 获取 easy-mock 的模拟数据
+            getData() {
+                // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
+                if (process.env.NODE_ENV === 'development') {
+                    this.url = '/ms/table/list';
+                };
+                this.$axios.post(this.url, {
+                    page: this.cur_page
+                }).then((res) => {
+                    this.tableData = res.data.list;
+                    console.log(this.tableData)
+                    console.log(this.tableData.slice((this.cur_page-1)*this.pagesize,this.cur_page*this.pagesize))
+
+                })
+            },
+            search() {
+                this.is_search = true;
+            },
+            formatter(row, column) {
+                return row.address;
+            },
+            filterTag(value, row) {
+                return row.tag === value;
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+    .del-dialog-cnt{
+        font-size: 16px;
+        text-align: center
+    }
+</style>
